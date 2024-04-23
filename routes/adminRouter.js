f593e2937b3db8237a7376a219ccbdf5c4ca0ddc
sahin-dev <sahinsiraj360@gmail.com>
2024-04-24 04:42:04 +0600

    Fix getAllTansport route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.route('/requests').get(protect, admin, getRequestsByResolve);
 router.route('/users').get(protect,admin, getAllUsers);
-router.route('/transports').get(protect,admin, getAllTransport);
+router.route('/vehicles').get(protect,admin, getAllTransport);
 router.route('/stopage').post(protect,admin, addStopage);
 router.route('/route').post(protect, admin, addRoute);
 router.route('/vehicle/approve').post(protect,admin,approveTransport);
