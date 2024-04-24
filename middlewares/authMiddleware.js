1d03d55c16970869ca66b20f7b93fcae308a767f
sahin-dev <sahinsiraj360@gmail.com>
2024-04-24 09:51:06 +0600

    Fix admin middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -23,7 +23,7 @@ const protect = asyncHandler(async (req, res, next) => {
 })
 
 const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
+  if (req.user && (req.user.role === 'admin')) {
     next()
   } else {
     next(createError(401,"Not authorized as an admin"));
